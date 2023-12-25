80d489abc8bf2d02767c2c0c2c055e07054543c7
danielpirela <danielac@gmail.com>
2023-12-24 23:23:22 -0400

    add prittier

--- src/components/BackArrow.tsx
+++ src/components/BackArrow.tsx
@@ -2,8 +2,19 @@ import Image from 'next/image'
 
 export const BackArrow = () => {
     return (
-        <button className='flex justify-center items-center w-auto h-auto' onClick={()=>        {history.back()}}>
-            <Image src={'/images/backArrow.png'} width={32} height={32} alt='back arrow icon'/>
+        <button
+            className="flex justify-center items-center w-auto h-auto"
+            onClick={() => {
+                history.back()
+            }}
+        >
+            <Image
+                src={'/images/backArrow.png'}
+                width={32}
+                height={32}
+                alt="back arrow icon"
+            />
         </button>
     )
 }
+
