3fef7989d9af77612d26383079876547fdd10c98
danielpirela <danielapirelac@gmail.com>
2024-01-22 16:52:40 -0800

    remove flowbite plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,21 +5,18 @@ const config: Config = {
         './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
         './src/components/**/*.{js,ts,jsx,tsx,mdx}',
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-        './node_modules/flowbite/**/*.js'
+        './node_modules/flowbite/**/*.js',
     ],
     theme: {
         extend: {
             backgroundImage: {
                 'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
                 'gradient-conic':
-                'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
             },
         },
     },
     darkMode: 'class',
-    plugins: [
-        require('tailwindcss-animated'),
-        require('flowbite/plugin')
-    ],
+    plugins: [require('tailwindcss-animated')],
 }
 export default config
